3284ea6174541619d19c61f58e31fc0d8e9f6418
schwarzfischer1776 <schwarzfischer.felix@web.de>
2020-10-15 22:26:57 +0200

    exercise_view

--- FSapp/views.py
+++ FSapp/views.py
@@ -2,16 +2,21 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 from FSapp.models import Exam, Exercise, Student, Professor
-
+from django.http import JsonResponse 
 def home(request):
     return render(request, 'FSapp/home.html')
 
-def set_of_exercises(request):
-
+def set_of_exercises(request): 
+    search = request.GET.get('search') if request.GET.get('search') != None else ''
+    print(search)
+    
     context ={
-        'exercises': Exercise.objects.all()
+        'exercises': Exercise.objects.filter(name__icontains=search)
     }
-    print(context['exercises'][0])
+    if request.is_ajax():
+        exercise_query(request)
+
+        #return JsonResponse(list(Exercise.objects.filter(name__icontains=search)), safe=False) 
     return render(request, 'FSapp/set_of_exercises.html', context=context)
 
 def favourites(request):
@@ -26,5 +31,8 @@ def exercise_view(request,pk):
     }
     return render(request, 'FSapp/exercise_view.html',context)
 
+def exercise_query(request):
+    print('sdfsdf')
 
+    return render(request, 'FSapp/exercise_query.html',context={})
 # Create your views here.

05a53389f12aeb580da5a94780c68cf5b1c390e9
RiaRosenauer <ria@rosenauer-layer.de>
2020-10-15 03:34:54 +0200

    adjusting the templates a little bit

--- FSapp/urls.py
+++ FSapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/admin', admin.site.urls),
-    path('/exerciseSet', views.set_of_exercises, name='set_of_exercises'),
-    path('/favourites', views.favourites, name='favourites'),
-    path('/toRepeat', views.to_repeat, name='to_repeat')
+    path('admin', admin.site.urls),
+    path('exerciseSet', views.set_of_exercises, name='set_of_exercises'),
+    path('favourites', views.favourites, name='favourites'),
+    path('toRepeat', views.to_repeat, name='to_repeat')
 ]

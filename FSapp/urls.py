56ea3901d40d8f367e71c2d191df0f0bb5d3d9cd
schwarzfischer1776 <schwarzfischer.felix@web.de>
2020-10-15 02:32:39 +0200

    Datenbank addiert

--- FSapp/urls.py
+++ FSapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('/admin', admin.site.urls),
 ]

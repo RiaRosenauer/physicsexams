f69e90bf4b064979d7eef8eb43653d315b78dc8a
RiaRosenauer <ria@rosenauer-layer.de>
2020-10-15 02:59:18 +0200

    adding Authentication

--- FSweb/settings.py
+++ FSweb/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Application definition
 
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'FSweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

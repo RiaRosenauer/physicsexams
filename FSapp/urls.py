0d8a6ca25c00a98a31b906bd0a504662ceaabd9b
RiaRosenauer <ria@rosenauer-layer.de>
2020-10-16 04:05:52 +0200

    adding the solved/ failed buttons

--- FSapp/urls.py
+++ FSapp/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('favourites', views.favourites, name='favourites'),
     path('toRepeat', views.to_repeat, name='to_repeat'),
     path('exerciseSet/<int:pk>',views.exercise_view, name='exercise_view'), 
+    path('exerciseSet/currentExercise_ajax',views.exercise_view_ajax, name='exercise_view_ajax'), 
 ]
--- FSapp/views.py
+++ FSapp/views.py
@@ -4,6 +4,11 @@ from django.utils.safestring import mark_safe
 from FSapp.models import Exam, Exercise, Student, Professor
 from django.http import JsonResponse 
 from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+
+
 def home(request):
     return render(request, 'FSapp/home.html')
 
@@ -27,9 +32,40 @@ def favourites(request):
 def to_repeat(request):
     return render(request, 'FSapp/to_repeat.html')
 
+@login_required
+def exercise_view_ajax(request):
+    solved = request.GET.get('solved')
+    exercise = request.GET.get('exercise')
+
+    student = Student.objects.filter(user=request.user)[0]
+
+    if solved == 'true':
+        student.solved_exercises.add(exercise)
+        student.failed_exercises.remove(exercise)
+    else:
+        student.solved_exercises.remove(exercise)
+        student.failed_exercises.add(exercise)
+    return JsonResponse({})
+
+@login_required
 def exercise_view(request,pk):
+    exercise = Exercise.objects.filter(pk=pk)[0]
+
+    student = Student.objects.filter(user=request.user)[0]
+    if exercise in student.solved_exercises.all():
+        solved = True
+        already_done = True
+    else:
+        if exercise in student.failed_exercises.all():
+            already_done = True
+            solved = False
+        else:
+            solved = False
+            already_done = False
     context={
-        'exercise':Exercise.objects.filter(pk=pk)[0]
+    'exercise': exercise,
+    'solved': solved,
+    'already_done': already_done,
     }
     return render(request, 'FSapp/exercise_view.html',context)
 
